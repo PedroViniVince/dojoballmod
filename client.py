ea3f9c38b8b35ed5be33b9bed3b92a18e11e45f4
Rui Faria <alexfaria10.2005@gmail.com>
2025-07-04 12:19:51 +0100

    final?

--- client.py
+++ client.py
@@ -63,11 +63,11 @@ background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 #baliza vermelha
 baliza = pygame.image.load('goal.png').convert_alpha()
 baliza = pygame.transform.rotate(baliza,90)
-baliza = pygame.transform.scale(baliza, (300, 500)) 
+baliza = pygame.transform.scale(baliza, (400, 500)) 
 
 baliza2 = pygame.image.load('goal.png').convert_alpha()
 baliza2 = pygame.transform.rotate(baliza2,270)
-baliza2 = pygame.transform.scale(baliza2, (300, 500)) 
+baliza2 = pygame.transform.scale(baliza2, (400, 500)) 
 pygame.font.init()
 font1 = pygame.font.SysFont("comic sans", 24)
 font2 = pygame.font.SysFont("comic sans",36)
@@ -96,34 +96,33 @@ while running:
         pygame.display.get_window_size()[0],
         pygame.display.get_window_size()[1],
     )
-    clock.tick(80)
+    clock.tick(60)
     send_data(client_socket, inputs)
     state = receive_data(client_socket)
 
-    #screen.fill("antiquewhite4")
     screen.blit(background,(0,0))
 
     #campo
-    pygame.draw.rect(screen, "antiquewhite2", (state.field_coords[0],
+    pygame.draw.rect(screen, "white", (state.field_coords[0],
                                                state.field_coords[1],
                                                state.field_coords[2]- state.field_coords[0],
-                                               state.field_coords[3] - state.field_coords[1]), 3)
+                                               state.field_coords[3] - state.field_coords[1]), 4)
+    
     field_x1, field_y1, field_x2, field_y2 = state.field_coords
     field_center_x = (field_x1 + field_x2) // 2
     pygame.draw.line(
         screen,
-        pygame.Color("antiquewhite2"),
+        pygame.Color("white"),
         (field_center_x, field_y1),
         (field_center_x, field_y2),
         width=3
     )
-
-    # Draw circle outline in the center of the field
+    
     field_center_y = (field_y1 + field_y2) // 2
-    circle_radius = 100  # Adjust as needed
+    circle_radius = 150
     pygame.draw.circle(
         screen,
-        pygame.Color("antiquewhite2"),
+        pygame.Color("white"),
         (field_center_x, field_center_y),
         circle_radius,
         width=3
@@ -136,33 +135,27 @@ while running:
         pygame.draw.circle(screen, color, (int(player.x), int(player.y)), 45)
         pygame.draw.circle(screen, coloroutline, (int(player.x), int(player.y)), 45, width=5)
         name = player.name if player.name else "Unknown"
-        name_text = font1.render(name, True, pygame.Color("white"))
+        name_text = font1.render(str(name), True, pygame.Color("white"))
         text_rect = name_text.get_rect(center=(int(player.x), int(player.y) - 50))
         screen.blit(name_text, text_rect)
-        pygame.draw.circle(screen, color, (int(player.x), int(player.y)), 45)
-        pygame.draw.circle(screen, coloroutline, (int(player.x), int(player.y)), 45, width=5)
         update_rects.append(pygame.Rect(player.x-45, player.y-45, 90, 90))
         update_rects.append(text_rect)
         
     # Postes
-    for post in state.posts.values():
-        pygame.draw.circle(screen, pygame.Color("cornsilk"), (int(post.x), int(post.y)), 15)
-        
     red_baliza_post = next((post for post in state.posts.values() if post.team == Team.RED), None)
     blue_baliza_post = next((post for post in state.posts.values() if post.team == Team.BLUE), None)
 
     if red_baliza_post:
-        baliza_rect = baliza.get_rect(center=(int(red_baliza_post.x-62), int(red_baliza_post.y + 150)))
+        baliza_rect = baliza.get_rect(center=(int(red_baliza_post.x-83), int(red_baliza_post.y + 150)))
         screen.blit(baliza, baliza_rect)
     if blue_baliza_post:
         # rodar baliza para a equipa azul
-        baliza_rect = baliza2.get_rect(center=(int(blue_baliza_post.x+62), int(blue_baliza_post.y + 150)))
+        baliza_rect = baliza2.get_rect(center=(int(blue_baliza_post.x+83), int(blue_baliza_post.y + 150)))
         screen.blit(baliza2, baliza_rect)
     
     # Bola
     pygame.draw.circle(screen, pygame.Color("white"), (int(state.ball.x), int(state.ball.y)), 30)
     update_rects.append(screen.get_rect())
-
 
     #menu
     pygame.draw.rect(screen, "antiquewhite4", [0, 0, 1920, 95])
@@ -178,7 +171,7 @@ while running:
         clock_state = "Break"
         pygame.draw.rect(screen, "antiquewhite4", [0, 500, 1920, 90])
         pygame.draw.rect(screen, "antiquewhite3", [0, 500, 1920, 90], 3)
-        screen.blit(font2.render("Break", True, pygame.Color("white")), (900, 520))
+        screen.blit(font2.render("Break", True, pygame.Color("white")), (960, 540))
     elif state.match_manager.state == MatchState.PAUSED:
         clock_state = "Paused"
     text_clock_surface = font2.render(f"{clock_state} : {clock_text}", True, pygame.Color("white"))
